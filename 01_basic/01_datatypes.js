2663d3e8202abbc6bf9fe7dabc4feb4d5dedbfd0
rockyverm <154210921+rockyverm@users.noreply.github.com>
2024-10-18 11:02:08 +0000

    datatype

---
+++ 01_basic/01_datatypes.js
@@ -0,0 +1,15 @@
+"use strict";// we write this to use new changes in javascript
+
+// if we are writing on different line then there is no need of semicolon
+
+let name = "Rocky" //string
+let employee = false   //boolean
+let age = 24  //number
+
+null //it is an object
+
+console.log(typeof "name");
+console.log(typeof null)
+console.log(typeof undefined)
+console.log(typeof employee);
+
