5395cfe81f144ad5451ed19baeeb9fad2f4485e9
rockyverm <154210921+rockyverm@users.noreply.github.com>
2024-10-18 11:21:15 +0000

    conversion of datatypes

---
+++ 01_basic/01_conversionOperation.js
@@ -0,0 +1,28 @@
+// if you want to convert anything to number datatype just keep inside Number(anything)
+//similarly to string datatype so just keep String(anything)
+
+// to boolean datatype so just keep Boolean(anything)
+
+//you can notice first character of Number,String,and Boolean should be capital
+
+let age = 24
+
+age = String(24)
+
+// console.log(age);
+// console.log(typeof age);
+
+age = Boolean(age)
+
+age = "Rocky"
+let lastname = ""
+
+age = Boolean(age);
+lastname = Boolean(lastname)
+
+// console.log(age);
+// console.log(typeof age);
+// console.log(lastname);
+// console.log(typeof lastname);
+let num = Number(null)
+console.log(num);
